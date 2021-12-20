d07155814850493c7bca0897ce94fcf2baec52e3
jcarrio <julio.carrio@gmail.com>
2021-12-20 19:57:12 -0300

    Commit inicial
    Primeiro commit

---
+++ Controllers/EquipeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ocean_ejogos_api.Controllers
+{
+    //padrão de rota, dominio/api/nomecontroller
+    [Route("api/[controller]")]
+    [ApiController]
+    // herança ControllerBase, heranças já prontas
+    public class EquipeController : Controller
+    {
+        // verbo (estará disponível para consulta)
+        [HttpGet]
+        public IActionResult qualquercoisa()
+        {
+            return Ok("Localizar equipes");
+        }
+
+/*
+        [HttpGet]
+        public IActionResult localizar()
+        {
+            return Ok("localizar as equipes");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+*/
+    }
+}
